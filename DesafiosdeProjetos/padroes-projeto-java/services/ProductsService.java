3820fb35744c90cf5a28b403056cbf903ba2cdbf
Jordan Cardek <jordancardek@hotmail.com>
2022-11-25 22:01:55 -0300

    Padrões de Projetos na Prática com Java

---
+++ DesafiosdeProjetos/padroes-projeto-java/services/ProductsService.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.labpadroesprojetojavaspring.services;
+
+import one.digitalinnovation.labpadroesprojetojavaspring.entities.Products;
+
+/**
+ *
+ */
+public interface ProductsService {
+
+    Iterable<Products> findAll();
+
+    Products findById(Long id);
+
+    void insert(Products product);
+
+    void update(Long id, Products product);
+
+    void delete(Long id);
+}
